1725ef65ff3cf66c2a047c124ce73bd761d27397
Abhiraj Dutta <abhirajdutta7@gmail.com>
2026-06-08 13:55:18 +0530

    Added gradients

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 #include "EBO.h"
 #include "VAO.h"
 
-const int WINDOW_WIDTH = 1920;
+const int WINDOW_WIDTH = 1080;
 const int WINDOW_HEIGHT = 1080;
 
 
 
-GLfloat vertices[] = {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
-	-0.5f / 2, 0.5f * float(sqrt(3))/ 6, 0.0f,
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
-	0.0f / 2, -0.5f * float(sqrt(3)) / 3, 0.0f,
+GLfloat vertices[] = {                                      // colors
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,                0.8f, 0.3f, 0.02f,
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,                 0.8f, 0.3f, 0.2f,
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,              1.0f, 0.6f, 0.32f,  
+	-0.5f / 2, 0.5f * float(sqrt(3))/ 6, 0.0f,              0.9f, 0.45f, 0.17f,
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,              0.9f, 0.45f, 0.17f,
+	0.0f / 2, -0.5f * float(sqrt(3)) / 3, 0.0f,             0.8f, 0.3f, 0.02f
 
 };
 
@@ -47,25 +47,28 @@ int main() {
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-
+	GLuint uniID = glGetUniformLocation(shader.ID, "scale");
 
 	// keep the window open
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
 		shader.Activate();
+		glUniform1f(uniID, 0.5f);
 
 		VAO1.Bind();
 
+
+
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
-
 		glfwSwapBuffers(window);
-
 		glfwPollEvents();
 
 	}
